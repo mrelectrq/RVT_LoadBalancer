2eefd878073b8bf344a4e43a46fcdd2e4503b082
mrelectrq <tiora100bass@gmail.com>
2020-12-15 12:50:58 +0200

    Configured Kestrel to require ClientCertificate

--- RVT.LoadBalancer.Application/Program.cs
+++ RVT.LoadBalancer.Application/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace RVT.LoadBalancer.Application
@@ -21,6 +23,15 @@ namespace RVT.LoadBalancer.Application
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(o =>
+                    {
+                        o.ConfigureHttpsDefaults(o => o.ClientCertificateMode = Microsoft.AspNetCore.Server.Kestrel.Https.ClientCertificateMode.RequireCertificate);
+                        o.Listen(IPAddress.Loopback, 5000);
+                        o.Listen(IPAddress.Loopback, 44322, listenOpt =>
+                        {
+                            listenOpt.UseHttps(Path.Combine("../Certs/loadbalancer.pfx"), "ar4iar4i",opt=>opt.AllowAnyClientCertificate());
+                        });
+                    });
                 });
     }
 }
