838157a839a60fe7393fdff83afef46e363e1ae5
mrelectrq <tiora100bass@gmail.com>
2020-12-17 15:17:26 +0200

    Excluded unnecessary values

--- RVT.Common/Messages/ChooserLbMessage.cs
+++ RVT.Common/Messages/ChooserLbMessage.cs
@@ -4,19 +4,11 @@ namespace RVT.Common.Messages
 {
     public class ChooserLbMessage
     {
-        public string IDNP { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+
         public string Gender { get; set; }
         public DateTime? Birth_date { get; set; }
         public DateTime Vote_date { get; set; }
         public int Region { get; set; }
-        public string Phone_Number { get; set; }
-        public string Email { get; set; }
-        /// <summary>
-        /// IDVN ID unic numeric a votantului
-        /// </summary>
-        /// 
         public string IDVN { get; set; }
         public int PartyChoosed { get; set; }
     }
